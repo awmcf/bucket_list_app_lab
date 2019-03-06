3aaba407f7b04cfe148c5bbe4bd3b791d3f0847b
awmcf <aaronmcfaull@gmail.com>
2019-03-06 10:59:01 +0000

    Added some seed data in seeds.js

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bucketList;
+db.dropDatabase();
+
+db.bucketList.insertMany([
+  {
+    activity: "Bungee Jump",
+    location: "Australia",
+    cost: "500"
+  },
+  {
+    activity: "Seeing the Taj Mahal",
+    location: "Agra, India",
+    cost: "2000"
+  },
+  {
+    activity: "Camp America",
+    location: "West Virginia",
+    cost: "1000"
+  }
+]);
